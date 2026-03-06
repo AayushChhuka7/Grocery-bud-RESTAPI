94fc82185631fcc3c7d0f3d3dddf441006734db9
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-03-06 22:07:50 +0545

    backend

---
+++ grocery-app/backend/grocery_project/urls.py
@@ -0,0 +1,10 @@
+
+
+# project level urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/grocery/', include('grocery.urls')),
+]
