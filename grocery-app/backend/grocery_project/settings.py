d8ffa3f9a13659d17cfc996ac339b3338f03cd58
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-03-08 17:06:51 +0545

    testing

--- grocery-app/backend/grocery_project/settings.py
+++ grocery-app/backend/grocery_project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        engine='django.db.backends.postgresql',
+        
     )
 }
 
